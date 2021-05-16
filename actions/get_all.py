4216685a448eb5642c026ac0f2e692221b1b62b5
Ejaz Shaikh <Ejaz.Shaikh@blueconchtech.com>
2021-05-16 22:44:58 +0530

    first

---
+++ actions/get_all.py
@@ -0,0 +1,16 @@
+import requests
+from st2common.runners.base_action import Action
+
+
+__all__ = ["GetAll"]
+
+
+class GetAll(Action):
+
+    def __init__(self, config) -> None:
+        super(GetAll, self).__init__(config)
+        self.url = self.config.get('base_url')
+
+    def run(self):
+        notes = requests.get(self.url)
+        return notes
